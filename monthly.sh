01ce98ae0745e3892b5f6d2d8caeafe0278ffdea
Oki Marzuqi <33563037+omodoong@users.noreply.github.com>
2020-03-31 11:53:07 +0700

    Update monthly.sh
    update comment

--- monthly.sh
+++ monthly.sh
@@ -4,10 +4,12 @@
 # if just want make 1 directory, write 1 line in dirlist.txt
 cat "./dirlist.txt" | while read nameDir
     do 
+        # handler for skip line using symbol "#"
         case "$nameDir" in \#*)
             continue
             ;;
         esac
+            # function create directory
             if [ ! -d "$nameDir/${YYYY}/${MM}" ]; 
                 then
                     mkdir -p "$nameDir/${YYYY}/${MM}"
